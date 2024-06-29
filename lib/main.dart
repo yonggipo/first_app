482adda844e1299907ced0aeec0e6ecf226c2126
yonggipo <yonggipo@icloud.com>
2024-06-29 17:47:56 +0900

    text color, font

--- lib/main.dart
+++ lib/main.dart
@@ -14,46 +14,54 @@ class MyApp extends StatelessWidget {
       title: 'Flutter First App',
       theme: ThemeData(useMaterial3: true),
       home: Scaffold(
-        // body: Container(
-        //   decoration: const BoxDecoration(
-        //     gradient: LinearGradient(
-        //       colors: [
-        //         Colors.yellow,
-        //         Colors.purple,
-        //       ],
-        //       begin: Alignment.topLeft,
-        //       end: Alignment.bottomRight,
-        //     ),
-        //   ),
-        //   child: const Text('Welcome to Flutter'),
-        // ),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.yellow,
+                Colors.purple,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Welcome to Flutter',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
         backgroundColor: Colors.deepPurple,
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: Container(
-          width: double.infinity,
-          padding: const EdgeInsets.all(12),
-          child: const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Flutter - The Complete Guide Course',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Learn Flutter step-by-step, from the ground up.',
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
-        ),
+        // body: Container(
+        //   width: double.infinity,
+        //   padding: const EdgeInsets.all(12),
+        //   child: const Column(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     crossAxisAlignment: CrossAxisAlignment.center,
+        //     children: [
+        //       Text(
+        //         'Flutter - The Complete Guide Course',
+        //         textAlign: TextAlign.center,
+        //         style: TextStyle(
+        //           fontSize: 24,
+        //           fontWeight: FontWeight.bold,
+        //         ),
+        //       ),
+        //       SizedBox(height: 16),
+        //       Text(
+        //         'Learn Flutter step-by-step, from the ground up.',
+        //         textAlign: TextAlign.center,
+        //       ),
+        //     ],
+        //   ),
+        // ),
       ),
     );
   }
