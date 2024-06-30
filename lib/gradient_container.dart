3d51ce67ba73a6ed35604d87bcd352338c3ceb09
yonggipo <yonggipo@icloud.com>
2024-06-30 14:23:27 +0900

    add alignment variable

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,20 +2,23 @@ import 'package:flutter/material.dart';
 
 import 'package:first_app/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Colors.yellow,
             Colors.purple,
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
